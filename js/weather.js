3c29efbca7565238c23d3e537f3c978552eb0ebb
KRDoubleS <tkdtn187@gmail.com>
2024-05-19 21:21:18 +0900

    240519-2

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,19 @@
+const API_KEY = "47792135b42b1407b652e41502ff0475";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   console.log("You live in", lat, ",", lon);
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃ / `;
+      city.innerText = data.name;
+    });
 }
 
 function onGeoErr() {
